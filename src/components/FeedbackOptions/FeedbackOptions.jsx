cff9e26509f643b5a46f7df717694b9b56e56c63
NataliiaVlasenko <108278401+NataliiaVlasenko@users.noreply.github.com>
2023-01-18 21:49:59 +0200

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -15,6 +15,6 @@ export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
 	);
 };
 FeedbackOptions.propTypes = {
-	options: PropTypes.array.isRequired,
+	options: PropTypes.arrayOf(PropTypes.string).isRequired,
 	onLeaveFeedback: PropTypes.func.isRequired
 };
